c58b214c722cc4764a8feac04513a1df57fdc477
richard <richard>
2024-02-01 11:28:29 +0000

    Added updates

--- sections/1-human-resources.js
+++ sections/1-human-resources.js
@@ -1,5 +1,24 @@
-function makeNameTags() {}
+function makeNameTags(guest){
+    const newObj = []
+    for(let i=0;i<guest.length;i++){
+        newObj.push(guest[i])
+        newObj[i].nameTag = `${guest[i].title} ${guest[i].forename} 
+        ${guest[i].surname}, ${guest[i].company}`
+    }  
+    return newObj
+}
 
-function createPoll() {}
+
+function createPoll(arr) {
+    let obj = {}
+    const tempArr = [...arr]
+    for (let i=0; i<tempArr.length;i++){
+        obj[tempArr] =    arr.filter(function(item){
+        if (item ===tempArr[0] ) 
+            return item
+        }).length
+   } 
+   return obj
+}
 
 module.exports = { makeNameTags, createPoll };
--- sections/3-closure.js
+++ sections/3-closure.js
@@ -1,5 +1,27 @@
-function generateMultiples() {}
+function generateMultiples(num) {
+    let arr = [], count = 0, increment = 0
+    return function (num2){
+        if (num === 0){
+            return []
+        }
+        for (i=1;i<num2;i++){
+            count=num*i
+            arr.push(count)
+            count = 0
+        }
+        return arr
+    }
+}
 
-function secureFunc() {}
+function secureFunc(password, func) {
+    return function (pass){
+        if (password === pass){
+            return func()
+        } else {
+            return "Sorry your password is incorrect"
+
+        }
+    }
+}
 
 module.exports = { generateMultiples, secureFunc };
